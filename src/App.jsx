9f6c5381ed86611b4ae764ce8eb55db9c6fdbfd6
LordShehzi <mohsshehroze7@gmail.com>
2026-03-30 17:33:40 +0500

    Initial React setup for Investmend Fund Tracker project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "./pages/Dashboard"
+
+function App() {
+
+  return (
+    <>
+      <h1>Investment Fund Tracker</h1>
+      <Dashboard/>
+    </>
+  )
+}
+
+export default App
